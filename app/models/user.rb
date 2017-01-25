a006c03e9f34bb76b214c5ff982a5ba8c9130549
Roman Pramberger <roman@pramberger.ch>
2017-01-25 20:43:24 +0000

    fix user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     :uniqueness => {
        :case_sensitive => false
     }
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validates_format_of :username, with: /\A[a-zA-Z0-9_\.]*\z/, :multiline => true
 
   after_create :user_created
   after_destroy :user_destroyed
