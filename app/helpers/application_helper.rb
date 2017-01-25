72df353c645100ceaf8ae903b6e3fa4293755ed9
Roman Pramberger <roman@pramberger.ch>
2017-01-25 20:53:45 +0000

    make metamagic helper nicer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
   # Make metamagic simpler by having inhertage for og and twitter tags
   def _meta options
-    options[:twitter] = {} if options[:twitter].nil?
-    options[:twitter][:title] = options[:title] if options[:twitter][:title].nil?
-    options[:twitter][:description] = options[:description] if options[:twitter][:description].nil? && !options[:description].nil?
-
-    options[:og] = {} if options[:og].nil?
-    options[:og][:title] = options[:title] if options[:og][:title].nil?
-    options[:og][:description] = options[:description] if options[:og][:description].nil? && !options[:description].nil?
+    [:twitter, :og].each do |tag|
+      options[tag] = {} if options[tag].nil?
+      options[tag][:title] = options[:title]
+      options[tag][:description] = options[:description]
+    end
 
     meta options
   end
